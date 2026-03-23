87f9de7eb79a9e545b256ce41f4920a17fec5d90
ta0ta0-star <taichi.northtail@gmail.com>
2026-03-23 21:42:20 +0900

    ディレクトリの仕様変更。メモ詳細画面追加

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,15 @@
 import { Slot } from 'expo-router'
 import React from 'react'
+import { useFonts } from 'expo-font'
 
-const Layout = (): React.JSX.Element => {
+const Layout = (): React.JSX.Element | null => {
+    const [loaded] = useFonts({
+        ...require('@expo/vector-icons/FontAwesome').font,
+        ...require('@expo/vector-icons/Feather').font,
+    })
+
+    if (!loaded) return null
+
     return <Slot />
 }
 
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,51 +1,8 @@
+import { Redirect } from 'expo-router'
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
-
-import Header from '../components/Header'
-import MemoListItem from '../components/MemoListItem'
-import CircleButton from '../components/CircleButton'
 
 const Index = (): React.JSX.Element => {
-    return (
-        <View style={styles.container}>
-            <Header />
-            <View>
-                <View>
-                    <MemoListItem />
-                    <MemoListItem />
-                    <MemoListItem />
-                </View>
-            </View>
-            <CircleButton>＋</CircleButton>
-        </View>
-    )
+    return <Redirect href='memo/edit' />
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor :'#ffffff'
-    },
-    circleButton: {
-        width: 64,
-        height: 64,
-        borderRadius: 32,
-        backgroundColor: '#467FD3',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        right: 40,
-        bottom: 40,
-        shadowColor: '#000000',
-        shadowOpacity: 0.25,
-        shadowRadius: 8,
-        shadowOffset: { width: 0, height: 8 },
-        elevation: 8
-    },
-    circleButtonLabel: {
-        color: "#ffffff",
-        fontSize: 40,
-    },
-})
 
 export default Index
