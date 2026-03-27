9fcac15fa7ef29097beaae535d11ef89be05da77
ta0ta0-star <taichi.northtail@gmail.com>
2026-03-27 19:45:36 +0900

    ユーザーごとにデータを保存できるように修正

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     View, StyleSheet, TextInput, KeyboardAvoidingView
 } from 'react-native'
 import { router } from 'expo-router'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
 
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
+import { db, auth } from '../../config'
 
-const handlePress = (): void => {
-    router.back()
+const handlePress = (bodyText: string): void => {
+    if (auth.currentUser === null) { return }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    addDoc(ref, {
+        bodyText,
+        updatedAt: Timestamp.fromDate(new Date())
+    })
+        .then((docRef) => {
+            console.log('success', docRef.id)
+            router.back()
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
 
 const Create = (): React.JSX.Element => {
+    const [bodyText, setBodyText] = useState('')
     return (
         <KeyboardAvoidingView behavior='height' style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value='' />
+                <TextInput
+                multiline
+                style={styles.input}
+                value={bodyText}
+                onChangeText={(text) => {setBodyText(text)}}
+                />
             </View>
-            <CircleButton onPress={handlePress} style={{ bottom: 120, top: 'auto' }}>
+            <CircleButton onPress={() => { handlePress(bodyText) }} style={{ bottom: 120, top: 'auto' }}>
                 <Icon name='check' size={40} color='#ffffff' />
             </CircleButton>
         </KeyboardAvoidingView>
