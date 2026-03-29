27041ddeef9856693345dc5aec2f6179c3f7305e
ta0ta0-star <taichi.northtail@gmail.com>
2026-03-30 00:10:28 +0900

    メモ一覧をスクロール表示できるように変更

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList } from 'react-native'
 import { router, useNavigation } from 'expo-router'
 import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 
@@ -43,11 +43,10 @@ const List = (): React.JSX.Element => {
     }, [])
     return (
         <View style={styles.container}>
-            <View>
-                <View>
-                    {memos.map((memo) => <MemoListItem memo={memo} />)}
-                </View>
-            </View>
+            <FlatList
+            data={memos}
+            renderItem={({ item }) => <MemoListItem memo={item} /> }
+            />
             <CircleButton onPress={handlePress}>
                 <Icon name='plus' size={40} color='#ffffff' />
             </CircleButton>
